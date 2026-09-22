fdd67172cb0c4fb68c980b53056dd9c2196483c1
rokkystudio <rokkystudio@gmail.com>
2026-09-22 16:02:23 +0300

    Move storage access above connection status

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.rokkystudio.dropme"
         minSdk = 26
         targetSdk = 37
-        versionCode = 43
-        versionName = "1.0.43"
+        versionCode = 44
+        versionName = "1.0.44"
     }
 
     buildTypes {
